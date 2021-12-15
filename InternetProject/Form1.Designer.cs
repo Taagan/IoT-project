9dc0127347b637bacfee3121e6a9a59a315b3b85
Taagan <Tim.wennerberg@gmail.com>
2021-12-15 21:47:38 +0100

    main

---
+++ InternetProject/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace InternetProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkboxONOFF = new System.Windows.Forms.CheckBox();
+            this.tbxRed = new System.Windows.Forms.TextBox();
+            this.tbxGreen = new System.Windows.Forms.TextBox();
+            this.tbxBlue = new System.Windows.Forms.TextBox();
+            this.tbxBrightness = new System.Windows.Forms.TextBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkboxSensor = new System.Windows.Forms.CheckBox();
+            this.tbxSensorDistance = new System.Windows.Forms.TextBox();
+            this.rbtnAway = new System.Windows.Forms.RadioButton();
+            this.rbtnWithin = new System.Windows.Forms.RadioButton();
+            this.richtbx = new System.Windows.Forms.RichTextBox();
+            this.btnRecieveMessage = new System.Windows.Forms.Button();
+            this.tbxPublish = new System.Windows.Forms.TextBox();
+            this.btnPublish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkboxONOFF
+            // 
+            this.checkboxONOFF.AutoSize = true;
+            this.checkboxONOFF.Location = new System.Drawing.Point(634, 65);
+            this.checkboxONOFF.Name = "checkboxONOFF";
+            this.checkboxONOFF.Size = new System.Drawing.Size(67, 17);
+            this.checkboxONOFF.TabIndex = 0;
+            this.checkboxONOFF.Text = "ON/OFF";
+            this.checkboxONOFF.UseVisualStyleBackColor = true;
+            // 
+            // tbxRed
+            // 
+            this.tbxRed.Location = new System.Drawing.Point(44, 158);
+            this.tbxRed.Name = "tbxRed";
+            this.tbxRed.Size = new System.Drawing.Size(100, 20);
+            this.tbxRed.TabIndex = 1;
+            this.tbxRed.Text = "R";
+            // 
+            // tbxGreen
+            // 
+            this.tbxGreen.Location = new System.Drawing.Point(190, 158);
+            this.tbxGreen.Name = "tbxGreen";
+            this.tbxGreen.Size = new System.Drawing.Size(100, 20);
+            this.tbxGreen.TabIndex = 2;
+            this.tbxGreen.Text = "G";
+            // 
+            // tbxBlue
+            // 
+            this.tbxBlue.Location = new System.Drawing.Point(332, 158);
+            this.tbxBlue.Name = "tbxBlue";
+            this.tbxBlue.Size = new System.Drawing.Size(100, 20);
+            this.tbxBlue.TabIndex = 3;
+            this.tbxBlue.Text = "B";
+            // 
+            // tbxBrightness
+            // 
+            this.tbxBrightness.Location = new System.Drawing.Point(601, 158);
+            this.tbxBrightness.Name = "tbxBrightness";
+            this.tbxBrightness.Size = new System.Drawing.Size(100, 20);
+            this.tbxBrightness.TabIndex = 4;
+            this.tbxBrightness.Text = "100%";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Timer",
+            "Rave",
+            "Rainbow"});
+            this.checkedListBox1.Location = new System.Drawing.Point(24, 344);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 5;
+            // 
+            // checkboxSensor
+            // 
+            this.checkboxSensor.AutoSize = true;
+            this.checkboxSensor.Location = new System.Drawing.Point(601, 271);
+            this.checkboxSensor.Name = "checkboxSensor";
+            this.checkboxSensor.Size = new System.Drawing.Size(86, 17);
+            this.checkboxSensor.TabIndex = 7;
+            this.checkboxSensor.Text = "SensorMode";
+            this.checkboxSensor.UseVisualStyleBackColor = true;
+            // 
+            // tbxSensorDistance
+            // 
+            this.tbxSensorDistance.Location = new System.Drawing.Point(601, 309);
+            this.tbxSensorDistance.Name = "tbxSensorDistance";
+            this.tbxSensorDistance.Size = new System.Drawing.Size(100, 20);
+            this.tbxSensorDistance.TabIndex = 8;
+            this.tbxSensorDistance.Text = "Distance";
+            // 
+            // rbtnAway
+            // 
+            this.rbtnAway.AutoSize = true;
+            this.rbtnAway.Location = new System.Drawing.Point(602, 344);
+            this.rbtnAway.Name = "rbtnAway";
+            this.rbtnAway.Size = new System.Drawing.Size(51, 17);
+            this.rbtnAway.TabIndex = 9;
+            this.rbtnAway.TabStop = true;
+            this.rbtnAway.Text = "Away";
+            this.rbtnAway.UseVisualStyleBackColor = true;
+            // 
+            // rbtnWithin
+            // 
+            this.rbtnWithin.AutoSize = true;
+            this.rbtnWithin.Location = new System.Drawing.Point(602, 381);
+            this.rbtnWithin.Name = "rbtnWithin";
+            this.rbtnWithin.Size = new System.Drawing.Size(55, 17);
+            this.rbtnWithin.TabIndex = 10;
+            this.rbtnWithin.TabStop = true;
+            this.rbtnWithin.Text = "Within";
+            this.rbtnWithin.UseVisualStyleBackColor = true;
+            // 
+            // richtbx
+            // 
+            this.richtbx.Location = new System.Drawing.Point(273, 302);
+            this.richtbx.Name = "richtbx";
+            this.richtbx.Size = new System.Drawing.Size(100, 96);
+            this.richtbx.TabIndex = 11;
+            this.richtbx.Text = "";
+            // 
+            // btnRecieveMessage
+            // 
+            this.btnRecieveMessage.Location = new System.Drawing.Point(284, 273);
+            this.btnRecieveMessage.Name = "btnRecieveMessage";
+            this.btnRecieveMessage.Size = new System.Drawing.Size(75, 23);
+            this.btnRecieveMessage.TabIndex = 12;
+            this.btnRecieveMessage.Text = "Recieve";
+            this.btnRecieveMessage.UseVisualStyleBackColor = true;
+            this.btnRecieveMessage.Click += new System.EventHandler(this.btnRecieveMessage_Click);
+            // 
+            // tbxPublish
+            // 
+            this.tbxPublish.Location = new System.Drawing.Point(44, 268);
+            this.tbxPublish.Name = "tbxPublish";
+            this.tbxPublish.Size = new System.Drawing.Size(100, 20);
+            this.tbxPublish.TabIndex = 13;
+            // 
+            // btnPublish
+            // 
+            this.btnPublish.Location = new System.Drawing.Point(56, 239);
+            this.btnPublish.Name = "btnPublish";
+            this.btnPublish.Size = new System.Drawing.Size(75, 23);
+            this.btnPublish.TabIndex = 14;
+            this.btnPublish.Text = "Publish";
+            this.btnPublish.UseVisualStyleBackColor = true;
+            this.btnPublish.Click += new System.EventHandler(this.btnPublish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnPublish);
+            this.Controls.Add(this.tbxPublish);
+            this.Controls.Add(this.btnRecieveMessage);
+            this.Controls.Add(this.richtbx);
+            this.Controls.Add(this.rbtnWithin);
+            this.Controls.Add(this.rbtnAway);
+            this.Controls.Add(this.tbxSensorDistance);
+            this.Controls.Add(this.checkboxSensor);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.tbxBrightness);
+            this.Controls.Add(this.tbxBlue);
+            this.Controls.Add(this.tbxGreen);
+            this.Controls.Add(this.tbxRed);
+            this.Controls.Add(this.checkboxONOFF);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkboxONOFF;
+        private System.Windows.Forms.TextBox tbxRed;
+        private System.Windows.Forms.TextBox tbxGreen;
+        private System.Windows.Forms.TextBox tbxBlue;
+        private System.Windows.Forms.TextBox tbxBrightness;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkboxSensor;
+        private System.Windows.Forms.TextBox tbxSensorDistance;
+        private System.Windows.Forms.RadioButton rbtnAway;
+        private System.Windows.Forms.RadioButton rbtnWithin;
+        private System.Windows.Forms.RichTextBox richtbx;
+        private System.Windows.Forms.Button btnRecieveMessage;
+        private System.Windows.Forms.TextBox tbxPublish;
+        private System.Windows.Forms.Button btnPublish;
+    }
+}
+
---
+++ InternetProject/Form1.cs
@@ -0,0 +1,155 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.ExtendedAuthenticationExchange;
+using MQTTnet.Client.Publishing;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Client.Unsubscribing;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InternetProject
+{
+    public partial class Form1 : Form
+    {
+        private static IManagedMqttClient client;
+        string url = "hairdresser.cloudmqtt.com";
+        string user = "ioyixfdx";
+        string password = "DbnczQ9NC224";
+        int port = 15591;
+        string myString = "string";
+        public Form1()
+        {
+            InitializeComponent();
+            Task connect = ConnectAsync(url, user, password, port, false);
+            client.UseConnectedHandler(e =>
+            {
+                Console.WriteLine("Connected successfully with MQTT Brokers.");
+            });
+            client.UseDisconnectedHandler(e =>
+            {
+                Console.WriteLine("Disconnected from MQTT Brokers.");
+            });
+
+            Task sub = SubscribeAsync("Grupp4OUT", 1);
+
+        }
+
+        public static async Task ConnectAsync(string uRL, string user, string password, int port, bool sSLPort)
+        {
+            string clientId = Guid.NewGuid().ToString();
+            string mqttURI = uRL;
+            string mqttUser = user;
+            string mqttPassword = password;
+            int mqttPort = port;
+            bool mqttSecure = sSLPort;
+
+          var messageBuilder = new MqttClientOptionsBuilder()
+            .WithClientId(clientId)
+            .WithCredentials(mqttUser, mqttPassword)
+            .WithTcpServer(mqttURI, mqttPort)
+            .WithCleanSession();
+
+            var options = mqttSecure
+              ? messageBuilder
+                .WithTls()
+                .Build()
+              : messageBuilder
+                .Build();
+
+            var managedOptions = new ManagedMqttClientOptionsBuilder()
+              .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+              .WithClientOptions(options)
+              .Build();
+
+            client = new MqttFactory().CreateManagedMqttClient();
+
+            await client.StartAsync(managedOptions);
+        }
+
+
+
+        /// <summary>
+        /// Publish Message.
+        /// </summary>
+        /// <param name="topic">Topic.</param>
+        /// <param name="payload">Payload.</param>
+        /// <param name="retainFlag">Retain flag.</param>
+        /// <param name="qos">Quality of Service.</param>
+        /// <returns>Task.</returns>
+        public static async Task PublishAsync(string topic, string payload, bool retainFlag = true, int qos = 1) =>
+        await client.PublishAsync(new MqttApplicationMessageBuilder()
+        .WithTopic(topic)
+        .WithPayload(payload)
+        .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+        .WithRetainFlag(retainFlag)
+        .Build());
+
+
+
+        /// <summary>
+        /// Subscribe topic.
+        /// </summary>
+        /// <param name="topic">Topic.</param>
+        /// <param name="qos">Quality of Service.</param>
+        /// <returns>Task.</returns>
+        public static async Task SubscribeAsync(string topic, int qos = 1) =>
+        await client.SubscribeAsync(new TopicFilterBuilder()
+        .WithTopic(topic)
+        .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+        .Build());
+
+
+
+        public void RecieveHandler()
+        {
+            client.UseApplicationMessageReceivedHandler(e =>
+            {
+                try
+                {
+                    string topic = e.ApplicationMessage.Topic;
+                    //string topic = "Grupp4OUT";
+                    if (string.IsNullOrWhiteSpace(topic) == false)
+                    {
+                        string payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                        Console.WriteLine($"Topic: {topic}. Message Received: {payload}");
+                        MethodInvoker inv = delegate
+                        {
+                            richtbx.Text = payload;
+
+                        };
+                        Invoke(inv);
+                        MessageBox.Show($"Topic: {topic}. Message Received: {payload}");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message, ex);
+                }
+            });
+        }
+
+        private void btnRecieveMessage_Click(object sender, EventArgs e)
+        {
+            RecieveHandler();
+        }
+
+        private void btnPublish_Click(object sender, EventArgs e)
+        {
+            Task Pub = PublishAsync("Grupp4OUT", tbxPublish.Text, true, 1);
+            //RecieveHandler();
+        }
+    }
+}
